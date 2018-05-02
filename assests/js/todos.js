fc21bbdb3edd3ca0a8b3a249ab45aea3cee27725
Jibin Philipose <philiposejibin@gmail.com>
2018-05-03 02:05:43 +0530

    Added functionality to add new tasks but deleting and striking is not working on the new elements which are added by append.

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -3,11 +3,19 @@ $("li").on("click", function() {
    $(this).toggleClass("completed");
 });
 
+//This will delete the task from thr to-do list 
 $("span").on("click", function(){
-   $(this).parent().fadeOut("fast", function(){
+   $(this).parent().fadeOut("slow", function(){
       //Now we are working with the parent() element 
       //which is the li so we can directly remove it from the DOM
       $(this).remove();
    });
 });
 
+//This will add new tasks on enter being pressed
+$("input[type=text]").on("keypress", function (event) {
+   if (event.which == 13) {
+      let value = $(this).val();
+      $("ul").append("<li><span>X</span> " +value +"</li>");
+   }
+});
