1367846a0d3ed2237b75606afd2a433f57b09d9a
Jibin Philipose <philiposejibin@gmail.com>
2018-05-04 02:29:16 +0530

    Fixed the bug (On adding new tasks it couldnt be striked or delted) by adding click listeners to the ul instead of applying them on li and span.

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,21 +1,22 @@
-//This will strike the li as completed.
-$("li").on("click", function() {
+// This will strike the li as completed.
+$("ul").on("click", "li", function(){
    $(this).toggleClass("completed");
 });
 
-//This will delete the task from thr to-do list 
-$("span").on("click", function(){
+// This will delete the task from thr to-do list 
+$("ul").on("click", "span", function(){
    $(this).parent().fadeOut("slow", function(){
-      //Now we are working with the parent() element 
-      //which is the li so we can directly remove it from the DOM
+      // Now we are working with the parent() element 
+      // which is the li so we can directly remove it from the DOM
       $(this).remove();
    });
 });
 
-//This will add new tasks on enter being pressed
-$("input[type=text]").on("keypress", function (event) {
-   if (event.which == 13) {
-      let value = $(this).val();
-      $("ul").append("<li><span>X</span> " +value +"</li>");
+// This will add new tasks on enter being pressed
+$("input[type='text']").on("keypress", function (event){
+   if (event.which == 13){
+      let todoText = $(this).val();
+      $(this).val("");
+      $("ul").append("<li><span>X</span> " + todoText + "</li>");
    }
 });
