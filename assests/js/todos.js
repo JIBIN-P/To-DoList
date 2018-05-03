fbb884635ffffb43c7eecff8ab3a4c08811a07db
Jibin Philipose <philiposejibin@gmail.com>
2018-05-04 02:59:25 +0530

    added more commnets for better code understanding.

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,10 +1,15 @@
 // This will strike the li as completed.
 $("ul").on("click", "li", function(){
+   //  We are adding listeners to the ul element since all
+   //  the li elements are not created yet and this will
+   //  cause failure so to overcome that we added listerners to ul.
    $(this).toggleClass("completed");
 });
 
 // This will delete the task from thr to-do list 
 $("ul").on("click", "span", function(){
+   //  We are adding listeners to the ul element since all span's
+   //  are not created yet.
    $(this).parent().fadeOut("slow", function(){
       // Now we are working with the parent() element 
       // which is the li so we can directly remove it from the DOM
